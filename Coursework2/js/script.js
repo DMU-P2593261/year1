aca325c0e4d2a09fa9e3ed861d0632a3deefa1b0
Samuel Hemmings <72464117+DMU-P2593261@users.noreply.github.com>
2021-04-27 10:14:53 +0100

    Update script.js

--- Coursework2/js/script.js
+++ Coursework2/js/script.js
@@ -1,3 +1,11 @@
+/*
+The code for the burger menu was taken from a tutorial on YouTube which can be found here: https://youtu.be/gXkqy0b4M5g
+The code was not directly copy and pasted but was typed by hand to help myself further understand the code and know what was happening as each line was added.
+Some of the code has been edited to fit the theme of my website such as a change in animation speed, colouring and the pixel width that it takes to activate the mobile view.
+JavaScript is definitely not my strongest point in coding but I understand this to a degree. The video was very clear in explaining how everything worked.
+
+*/
+
 /*Main Function*/
 const burgerAnimation = () => { /*creates a function which is equal to an anonymous function*/
   const burger = document.querySelector('.burgerMenu'); /*creates a function which is equal to the burgerMenu class*/
